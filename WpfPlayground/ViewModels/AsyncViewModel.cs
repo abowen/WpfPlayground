4519204acb03aa56e15e7d6ce5f7b18dbf36e793
abowen <andrew.james.bowen@gmail.com>
2013-08-19 21:29:05 +1000

    Added another asynchronous feature using Task

--- WpfPlayground/ViewModels/AsyncViewModel.cs
+++ WpfPlayground/ViewModels/AsyncViewModel.cs
@@ -1,46 +1,99 @@
-﻿using System.Threading;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
-using System.Windows;
 using Caliburn.Micro;
 
 namespace WpfPlayground.ViewModels
 {
     public class AsyncViewModel : PropertyChangedBase
     {
-        private decimal _longProcessProgressBarProcess;
+        private decimal _asyncProgressBarProcess;
 
-        public decimal LongProcessProgressBarProcess
+        public decimal AsyncProgressBarProcess
         {
-            get { return _longProcessProgressBarProcess; }
+            get { return _asyncProgressBarProcess; }
             set
             {
-                _longProcessProgressBarProcess = value;
-                NotifyOfPropertyChange(() => LongProcessProgressBarProcess);
+                _asyncProgressBarProcess = value;
+                NotifyOfPropertyChange(() => AsyncProgressBarProcess);
             }
         }
 
-        public void LongProcessButton()
+        public void AsyncButton()
         {
-            StartLongProcess();
-            // TODO: Move to Notification Area
-            MessageBox.Show("Long Process Started");
+            StartAsync();            
         }
 
-        private async void StartLongProcess()
+        private async void StartAsync()
         {
-            await Task.Run(() => LongProcess());
-            // TODO: Move to Notification Area
-            MessageBox.Show("Long Process Finished");
+            await Task.Run(() => AsyncProcess());            
         }
 
-        private void LongProcess()
+        private void AsyncProcess()
         {
-            LongProcessProgressBarProcess = 0m;
-            while (LongProcessProgressBarProcess < 100)
+            AsyncProgressBarProcess = 0m;
+            while (AsyncProgressBarProcess < 100)
             {
                 Thread.Sleep(100);
-                LongProcessProgressBarProcess += 0.5m;
+                AsyncProgressBarProcess += 0.5m;
             }
         }
+
+        public void TaskButton()
+        {
+            TaskProcess();
+        }
+
+        private void TaskProcess()
+        {
+            var taskSquareRoot = Task<List<double>>.Factory.StartNew(() =>
+            {
+                SquareRootCount = 0;
+                var results = new List<double>();
+                for (int i = 1; i <= 10000; i++)
+                {
+                    var root = Math.Sqrt(i);
+                    var equalizer = (root%1);
+                    if (equalizer == 0)
+                    {
+                        results.Add(root);
+                        SquareRootNumber = i;
+                        Thread.Sleep(100);
+                    }
+                }
+                return results;
+            });
+
+            taskSquareRoot.ContinueWith(task =>                     
+            {
+                SquareRootCount = task.Result.Count;
+            });   
+         
+                        
+        }
+
+        private double _squareRootNumber;
+        public double SquareRootNumber
+        {
+            get { return _squareRootNumber; }
+            set
+            {
+                _squareRootNumber = value;
+                NotifyOfPropertyChange(() => SquareRootNumber);
+            }
+        }
+
+        private int _squareRootCount;
+        public int SquareRootCount
+        {
+            get { return _squareRootCount; }
+            set
+            {
+                _squareRootCount = value;
+                NotifyOfPropertyChange(() => SquareRootCount);
+            }
+        }
+
     }
 }
