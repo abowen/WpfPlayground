4e6c8feabeacf6c56014505f9c9bdb389f71c270
abowen <andrew.james.bowen@gmail.com>
2013-12-07 18:44:08 +1100

    Added Fade in & Fade out based off property

--- WpfPlayground/ViewModels/AnimationViewModel.cs
+++ WpfPlayground/ViewModels/AnimationViewModel.cs
@@ -3,6 +3,18 @@
 namespace WpfPlayground.ViewModels
 {
     public class AnimationViewModel : PropertyChangedBase
-    {      
+    {
+        private bool _isBusy;
+
+        public bool IsBusy
+        {
+            get { return _isBusy; }
+            set { _isBusy = value; NotifyOfPropertyChange(() => IsBusy); }
+        }
+
+        public void FadeButton()
+        {
+            IsBusy = !IsBusy;
+        }
     }
 }
